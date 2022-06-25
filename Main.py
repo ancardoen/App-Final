634ec7419b73c41f59916011081c2bcbc1e36327
Antonio Cardoen <ancardoen@alumnos.uai.cl>
2022-06-25 11:17:44 -0400

    try 2

--- Main.py
+++ Main.py
@@ -1,18 +1,10 @@
-from scrapy.item import Field
-from scrapy.item import Item
-from scrapy.spiders import CrawlSpider, Rule
+import requests
+from bs4 import BeautifulSoup
 
-class Articulo(Item):
-    titulo = Field()
-    precio = Field()
-    descripcion = Field()
 
-class MercadoLibreCrawler(CrawlSpider):
-    name = 'mercadolibre'
-    custom_settings = {
-        'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0'
-        'CLOSESPIDER_PAGECOUNT': 3
-    }
-    download_delay = 1
-    alowed_domains = ["listado.mercadolibre.cl"]
-    start_urls = ["https://listado.mercadolibre.cl/comida-erizo"]
+URL = "https://www.cruzverde.cl/search?query=paracetamol"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find_all("span", {"class": ["font-bold", "text-orange"]})
+print(results)
+
